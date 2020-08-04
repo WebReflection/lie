524bc621ba3724231b2840f295a1f1ebe19b9684
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-08-04 12:46:17 +0200

    passing the resolved value along

--- cjs/index.js
+++ cjs/index.js
@@ -1,12 +1,13 @@
 'use strict';
 module.exports = typeof Promise === 'function' ? Promise : function (fn) {
-  let queue = [], resolved = 0;
-  fn(() => {
+  let queue = [], resolved = 0, value;
+  fn($ => {
+    value = $;
     resolved = 1;
     queue.splice(0).forEach(then);
   });
   return {then};
   function then(fn) {
-    return (resolved ? setTimeout(fn) : queue.push(fn)), this;
+    return (resolved ? setTimeout(fn, 0, value) : queue.push(fn)), this;
   }
 };
